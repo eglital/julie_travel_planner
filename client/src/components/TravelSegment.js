0e13ef5b1ea9729ca0d3ce1f6dc060a16934e605
Renzo Tomlinson <rttomlinson@gmail.com>
2017-06-14 20:40:58 +0000

    Add component skeletons

---
+++ client/src/components/TravelSegment.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+
+const TravelSegment = () => {
+ return (
+        <div>
+             <div>Direction Arrow</div>
+             <div>Vehicle Icon and Duration</div>
+        </div>
+     );   
+};
+
+TravelSegment.propTypes = {
+    
+};
+
+
+export default TravelSegment;
