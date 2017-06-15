669903353c54d87ab0df365364ed9b86b8296743
Egle Libby <eglelibby@gmail.com>
2017-06-15 15:39:55 -0400

    merge

--- client/src/actions/locationsActions.js
+++ client/src/actions/locationsActions.js
@@ -1,12 +1,12 @@
-import 'isomorphic-fetch';
+import "isomorphic-fetch";
 
 import {
   SET_FETCHING,
   FETCH_LOCATIONS_DATA_SUCCESS,
   FETCH_LOCATIONS_DATA_FAILURE
-} from './types';
-import ApiResponseHelper from '../helpers/apiResponseHelper';
-import { setItineraryData } from './itineraryActions.js';
+} from "./types";
+import ApiResponseHelper from "../helpers/apiResponseHelper";
+import { setItineraryData } from "./itineraryActions.js";
 
 export function setFetching() {
   return {
@@ -29,44 +29,36 @@ export function fetchLocationsDataFailure(error) {
 }
 
 export function fetchLocationsData(form) {
-
-    return dispatch => {
-        const myHeaders = new Headers({
-            "Content-Type": "application/json"
-        });
-        const options = {
-            method: "POST",
-            headers: myHeaders,
-            body: JSON.stringify(form)
-        };
-
-        return fetch(
-            "https://julie-server.herokuapp.com/api/itinerary/start",
-            options
-        )
-            .then(ApiResponseHelper.responseChecker)
-            .then(ApiResponseHelper.parseToJSON)
-            .then(data => {
-                let itinerary = data.itinerary;
-                //remove from data object
-                delete data.itinerary;
-                //update the locations reducer
-                console.log("dispatching location success", data);
-
-                dispatch(setItineraryData(itinerary));
-
-                dispatch(fetchLocationsDataSuccess(data.locations));
-            })
-            .catch(err => {
-                console.log("dispatching location failure", err);
-                dispatch(fetchLocationsDataFailure(err));
-            });
-
   return dispatch => {
     const myHeaders = new Headers({
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     });
     const options = {
-      method: 'POST',
+      method: "POST",
       headers: myHeaders,
       body: JSON.stringify(form)
+    };
+
+    return fetch(
+      "https://julie-server.herokuapp.com/api/itinerary/start",
+      options
+    )
+      .then(ApiResponseHelper.responseChecker)
+      .then(ApiResponseHelper.parseToJSON)
+      .then(data => {
+        let itinerary = data.itinerary;
+        //remove from data object
+        delete data.itinerary;
+        //update the locations reducer
+        console.log("dispatching location success", data);
+
+        dispatch(setItineraryData(itinerary));
+
+        dispatch(fetchLocationsDataSuccess(data.locations));
+      })
+      .catch(err => {
+        console.log("dispatching location failure", err);
+        dispatch(fetchLocationsDataFailure(err));
+      });
+  };
+}
--- client/src/components/LocationSelection.js
+++ client/src/components/LocationSelection.js
@@ -1,43 +1,38 @@
-import React from 'react';
-import { Card, CardBlock, CardTitle } from 'reactstrap';
-import Dotdotdot from 'react-dotdotdot';
+import React from "react";
+import { Card, CardBlock, CardTitle } from "reactstrap";
+import Dotdotdot from "react-dotdotdot";
 const LocationSelection = props => {
   const { location, section, itineraryId, onClick } = props;
   if (!location.photo) {
-    location.photo = './128.png';
+    location.photo = "./128.png";
   }
 
   return (
     <Card
-      style={{ marginBottom: '10px', maxWidth: '500px' }}
+      style={{ marginBottom: "10px", maxWidth: "500px" }}
       onClick={onClick}
       data-loc={JSON.stringify(location)}
       data-section={section}
       data-itinerary-id={itineraryId}
     >
-      <CardBlock style={{ padding: '10px' }}>
+      <CardBlock style={{ padding: "10px" }}>
         <div
           style={{
-            display: 'inline-block',
-<<<<<<< HEAD
-            backgroundImage: `url(${location.photo})`,
-=======
-            display: 'inline-block',
-            backgroundImage: `url(${location.photo || 'nophoto'})`,
->>>>>>> renzo-overviews
-            backgroundPosition: '50% 50%',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: '150px',
-            width: '33%',
-            height: '100px',
-            float: 'left',
-            marginRight: '5%'
+            display: "inline-block",
+            backgroundImage: `url(${location.photo || "nophoto"})`,
+            backgroundPosition: "50% 50%",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "150px",
+            width: "33%",
+            height: "100px",
+            float: "left",
+            marginRight: "5%"
           }}
         />
-        <div style={{ display: 'inline-block', float: 'left', width: '60%' }}>
+        <div style={{ display: "inline-block", float: "left", width: "60%" }}>
           <CardTitle
             className="text-center"
-            style={{ fontSize: '18px', marginTop: '-2px', marginBottom: '2px' }}
+            style={{ fontSize: "18px", marginTop: "-2px", marginBottom: "2px" }}
           >
             <Dotdotdot clamp={1}>
               {location.name}
@@ -45,9 +40,9 @@ const LocationSelection = props => {
           </CardTitle>
           <div
             style={{
-              fontSize: '14px',
-              height: '81px',
-              overflow: 'hidden'
+              fontSize: "14px",
+              height: "81px",
+              overflow: "hidden"
             }}
           >
             <Dotdotdot clamp={4}>
