338edcc7a64fb7bc19f7da8d38836a7a389a9c94
Nicholas Romeo <romeonicholas@gmail.com>
2017-06-15 09:02:53 -0700

    Add loader after form submission, update actions

--- client/src/actions/locationsActions.js
+++ client/src/actions/locationsActions.js
@@ -1,54 +1,61 @@
-import "isomorphic-fetch";
+import 'isomorphic-fetch';
 
 import {
-    FETCH_LOCATIONS_DATA_SUCCESS,
-    FETCH_LOCATIONS_DATA_FAILURE
-} from "./types";
-import ApiResponseHelper from "../helpers/apiResponseHelper";
-import { setItineraryData } from "./itineraryActions.js";
+  SET_FETCHING,
+  FETCH_LOCATIONS_DATA_SUCCESS,
+  FETCH_LOCATIONS_DATA_FAILURE
+} from './types';
+import ApiResponseHelper from '../helpers/apiResponseHelper';
+import { setItineraryData } from './itineraryActions.js';
+
+export function setFetching() {
+  return {
+    type: SET_FETCHING
+  };
+}
 
 export function fetchLocationsDataSuccess(data) {
-    return {
-        type: FETCH_LOCATIONS_DATA_SUCCESS,
-        data
-    };
+  return {
+    type: FETCH_LOCATIONS_DATA_SUCCESS,
+    data
+  };
 }
 
 export function fetchLocationsDataFailure(error) {
-    return {
-        type: FETCH_LOCATIONS_DATA_FAILURE,
-        error
-    };
+  return {
+    type: FETCH_LOCATIONS_DATA_FAILURE,
+    error
+  };
 }
 
 export function fetchLocationsData(form) {
-    return dispatch => {
-        const myHeaders = new Headers({
-            "Content-Type": "application/json"
-        });
-        const options = {
-            method: "POST",
-            headers: myHeaders,
-            body: JSON.stringify(form)
-        };
-
-        return fetch("/api/itinerary/start", options)
-            .then(ApiResponseHelper.responseChecker)
-            .then(ApiResponseHelper.parseToJSON)
-            .then(data => {
-                let itinerary = data.itinerary;
-                //remove from data object
-                delete data.itinerary;
-                //update the locations reducer
-                console.log("dispatching location success", data);
-
-                dispatch(setItineraryData(itinerary));
-
-                dispatch(fetchLocationsDataSuccess(data.locations));
-            })
-            .catch(err => {
-                console.log("dispatching location failure", err);
-                dispatch(fetchLocationsDataFailure(err));
-            });
+  return dispatch => {
+    const myHeaders = new Headers({
+      'Content-Type': 'application/json'
+    });
+    const options = {
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify(form)
     };
+
+    return fetch('/api/itinerary/start', options)
+      .then(ApiResponseHelper.responseChecker)
+      .then(ApiResponseHelper.parseToJSON)
+      .then(data => {
+        let itinerary = data.itinerary;
+        //remove from data object
+        delete data.itinerary;
+        //update the locations reducer
+        console.log('dispatching location success', data);
+
+        dispatch(setItineraryData(itinerary));
+
+        dispatch(fetchLocationsDataSuccess(data.locations));
+      })
+      .catch(err => {
+        console.log('dispatching location failure', err);
+        dispatch(fetchLocationsDataFailure(err));
+      });
+  };
 }
--- client/src/containers/InitialSubmissionFormContainer.js
+++ client/src/containers/InitialSubmissionFormContainer.js
@@ -1,146 +1,155 @@
-import React, { Component } from "react";
-import InitialSubmissionForm from "../components/InitialSubmissionForm";
-import { fetchLocationsData } from "../actions/locationsActions";
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
-import ItineraryHelper from "../helpers/itineraryHelper";
+import React, { Component } from 'react';
+import InitialSubmissionForm from '../components/InitialSubmissionForm';
+import { fetchLocationsData, setFetching } from '../actions/locationsActions';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import ItineraryHelper from '../helpers/itineraryHelper';
+import '../stylesheets/loading.css';
 
 function getNextHour() {
-    let ROUNDING = 60 * 60 * 1000; /*ms*/
-    let start = Date.now();
-    return Math.ceil(+start / ROUNDING) * ROUNDING;
+  let ROUNDING = 60 * 60 * 1000; /*ms*/
+  let start = Date.now();
+  return Math.ceil(+start / ROUNDING) * ROUNDING;
 }
 
+const Loader = () => <div className="loader">Loading...</div>;
+
 class InitialSubmissionFormContainer extends Component {
-    constructor() {
-        super();
-        this.state = {
-            nextHour: getNextHour(),
-            startTime: getNextHour(),
-            endTime: getNextHour() + 2 * 60 * 60 * 1000,
-            startingLocation: null,
-            error: null,
-            validItinerary: false
-        };
-    }
-    
-    componentDidMount(){
-        //check localStorage for itinerary: id
-        console.log("component did mount");
-        if (ItineraryHelper.validItinerary()){
-            this.setState({
-                validItinerary: ItineraryHelper.getItineraryObj()
-            });
-        } else {
-            this.setState({
-                validItinerary: false
-            });
-        }
-    }
-    
-    componentWillReceiveProps(newProps) {
-        //if locations.data is now populated, redirect them to itinerary-creation
-        if (Object.keys(newProps.locations.data).length > 0) {
-            this.props.history.push("/itinerary-creation");
-        }
-        //if error in form
-        if (newProps.locations.error) {
-            this.setState({
-                error: newProps.locations.error
-            });
-        }
-    }
-
-    onStartTimeChange = e => {
-        //if the endTime would be less than two hours after the new startTime
-        //advance it to at least two hours
-        if (this.state.endTime - +e.target.value < 2) {
-            this.setState({
-                startTime: +e.target.value,
-                endTime: +e.target.value + 2
-            });
-        } else {
-            this.setState({
-                startTime: +e.target.value
-            });
-        }
+  constructor() {
+    super();
+    this.state = {
+      nextHour: getNextHour(),
+      startTime: getNextHour(),
+      endTime: getNextHour() + 2 * 60 * 60 * 1000,
+      startingLocation: null,
+      error: null,
+      validItinerary: false
     };
+  }
 
-    onEndTimeChange = e => {
-        this.setState({
-            endTime: +e.target.value
+  componentDidMount() {
+    //check localStorage for itinerary: id
+    console.log('component did mount');
+    if (ItineraryHelper.validItinerary()) {
+      this.setState({
+        validItinerary: ItineraryHelper.getItineraryObj()
+      });
+    } else {
+      this.setState({
+        validItinerary: false
+      });
+    }
+  }
+
+  componentWillReceiveProps(newProps) {
+    //if locations.data is now populated, redirect them to itinerary-creation
+    if (Object.keys(newProps.locations.data).length > 0) {
+      this.props.history.push('/itinerary-creation');
+    }
+    //if error in form
+    if (newProps.locations.error) {
+      this.setState({
+        error: newProps.locations.error
+      });
+    }
+  }
+
+  onStartTimeChange = e => {
+    //if the endTime would be less than two hours after the new startTime
+    //advance it to at least two hours
+    if (this.state.endTime - +e.target.value < 2) {
+      this.setState({
+        startTime: +e.target.value,
+        endTime: +e.target.value + 2
+      });
+    } else {
+      this.setState({
+        startTime: +e.target.value
+      });
+    }
+  };
+
+  onEndTimeChange = e => {
+    this.setState({
+      endTime: +e.target.value
+    });
+  };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.setFetching();
+
+    //construct simple json for form submission
+    let data = {
+      startTime: this.state.startTime,
+      endTime: this.state.endTime
+    };
+    //attempt to get location with geolocation API
+    if ('geolocation' in navigator) {
+      /* geolocation is available */
+
+      let p = new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          resolve([position.coords.latitude, position.coords.longitude]);
+        }, reject);
+      });
+      p
+        .then(
+          coordinates => {
+            data.startingLocation = coordinates;
+          },
+          navError => {
+            //prompt with box for starting location and update the state?
+            console.log('Please enter a starting location');
+            data.startingLocation = this.state.startingLocation; //or default values?
+          }
+        )
+        .then(form => {
+          console.log('updated data', data);
+          //send form to action dispatcher
+          this.props.fetchLocationsData({
+            formSubmission: data
+          });
         });
-    };
-
-    onFormSubmit = e => {
-        e.preventDefault();
-        //construct simple json for form submission
-        let data = {
-            startTime: this.state.startTime,
-            endTime: this.state.endTime
-        };
-        //attempt to get location with geolocation API
-        if ("geolocation" in navigator) {
-            /* geolocation is available */
-
-            let p = new Promise((resolve, reject) => {
-                navigator.geolocation.getCurrentPosition(function(position) {
-                    resolve([
-                        position.coords.latitude,
-                        position.coords.longitude
-                    ]);
-                }, reject);
-            });
-            p
-                .then(
-                    coordinates => {
-                        data.startingLocation = coordinates;
-                    },
-                    navError => {
-                        //prompt with box for starting location and update the state?
-                        console.log("Please enter a starting location");
-                        data.startingLocation = this.state.startingLocation; //or default values?
-                    }
-                )
-                .then(form => {
-                    console.log("updated data", data);
-                    //send form to action dispatcher
-                    this.props.fetchLocationsData({
-                        formSubmission: data
-                    });
-                });
-        } else {
-            /* geolocation IS NOT available */
-        }
-    };
-    render() {
-        //create new rounded time to pass to submission form each time
-        //consider moving to lifecycle hook to check for changes to avoid rerenders
-        return (
-            <InitialSubmissionForm
-                onSubmit={this.onFormSubmit}
-                onStartTimeChange={this.onStartTimeChange}
-                onEndTimeChange={this.onEndTimeChange}
-                {...this.state}
-            />
-        );
+    } else {
+      /* geolocation IS NOT available */
     }
+  };
+  render() {
+    if (this.props.locations.isFetching) {
+      return <Loader />;
+    } else {
+      //create new rounded time to pass to submission form each time
+      //consider moving to lifecycle hook to check for changes to avoid rerenders
+      return (
+        <InitialSubmissionForm
+          onSubmit={this.onFormSubmit}
+          onStartTimeChange={this.onStartTimeChange}
+          onEndTimeChange={this.onEndTimeChange}
+          {...this.state}
+        />
+      );
+    }
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        locations: state.locations
-    };
+  return {
+    locations: state.locations
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        fetchLocationsData: form => {
-            dispatch(fetchLocationsData(form));
-        }
-    };
+  return {
+    fetchLocationsData: form => {
+      dispatch(fetchLocationsData(form));
+    },
+    setFetching: () => {
+      dispatch(setFetching());
+    }
+  };
 }
 
 export default withRouter(
-    connect(mapStateToProps, mapDispatchToProps)(InitialSubmissionFormContainer)
+  connect(mapStateToProps, mapDispatchToProps)(InitialSubmissionFormContainer)
 );
