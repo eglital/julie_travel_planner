44a6e1a800562f5c349f43b8f8efbdad2a3b6b07
Nicholas Romeo <romeonicholas@gmail.com>
2017-06-15 11:12:29 -0700

    Add hero and some styling to form

--- client/src/components/InitialSubmissionForm.js
+++ client/src/components/InitialSubmissionForm.js
@@ -1,76 +1,153 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Form, FormGroup, Label, Input, Col, Row, Alert, UncontrolledTooltip } from 'reactstrap';
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Col,
+  Row,
+  Alert,
+  UncontrolledTooltip
+} from 'reactstrap';
 import moment from 'moment';
+import Hero from './Hero';
 
 function createTimeOptions(time, startOffset = 0) {
-    //change to milli
-    let hours = getHoursInMilliseconds(offsetTime(time, startOffset));
-    return hours.map((hour) => {
-        return (
-            <option key={hour} value={hour}>{moment(hour).format('MMMM Do YYYY, h:mm:ss a')}</option>    
-        );
-    });
+  //change to milli
+  let hours = getHoursInMilliseconds(offsetTime(time, startOffset));
+  return hours.map(hour => {
+    return (
+      <option key={hour} value={hour}>
+        {moment(hour).format('h a')}
+      </option>
+    );
+  });
 }
 
-
 function getHoursInMilliseconds(nextHour) {
-    //warp in moment for functionality
-    nextHour = moment(nextHour);
-    //create an array of times in milliseconds starting with the hour until 22:00 of the same day
-    let hours = [];
-    let i = 0;
-    while (i < 12) {
-        //push the time in milliseconds
-        hours.push(+nextHour);
-        //mutate the moment
-        nextHour.add(1, 'h');
-        ++i;
-    }
-    return hours;
+  //warp in moment for functionality
+  nextHour = moment(nextHour);
+  //create an array of times in milliseconds starting with the hour until 22:00 of the same day
+  let hours = [];
+  let i = 0;
+  while (i < 12) {
+    //push the time in milliseconds
+    hours.push(+nextHour);
+    //mutate the moment
+    nextHour.add(1, 'h');
+    ++i;
+  }
+  return hours;
 }
 
 function offsetTime(time, startOffset) {
-    let hoursInMilliseconds = startOffset * 60 * 60 * 1000;
-    return time + +hoursInMilliseconds;
+  let hoursInMilliseconds = startOffset * 60 * 60 * 1000;
+  return time + +hoursInMilliseconds;
 }
 
 class InitialSubmissionForm extends Component {
-    
-    
   render() {
-      
-      const { startTime, nextHour, onStartTimeChange, onEndTimeChange, onSubmit, error, validItinerary } = this.props;
-    console.log("validItinerary", validItinerary);
+    const {
+      startTime,
+      nextHour,
+      onStartTimeChange,
+      onEndTimeChange,
+      onSubmit,
+      error,
+      validItinerary
+    } = this.props;
+    console.log('validItinerary', validItinerary);
     return (
-        <Row>
-            <Col xs="12" sm={{offset: 3, size: 6}}>
-            {error && <Alert color="warning"><strong>Whoops!</strong> Something happened on the server. Try again later</Alert> }
-            {validItinerary && <Alert color="info"><strong>You already have an itinerary saved!</strong><Link to={`/itinerary-overview/${validItinerary.id}`}>Click here to view</Link></Alert> }  
-              <Form onSubmit={onSubmit}>
-                <FormGroup>
-                  <Label for="startingLocation">Starting Location</Label>
-                  <Input type="text" name="startingLocation" id="startingLocation" placeholder="Use current location" />
-                </FormGroup>
-                <FormGroup>
-                  <Label for="startingTime">Select Starting Time</Label>
-                  <Input type="select" name="startingTime" id="startingTime" onChange={onStartTimeChange}>
-                    {createTimeOptions(nextHour, 0)}
-                  </Input>
-                </FormGroup>
-                <FormGroup>
-                  <Label for="endingTime">Select Ending Time</Label>
-                  <Input type="select" name="endingTime" id="endingTime" onChange={onEndTimeChange}>
-                    {createTimeOptions(startTime, 2)}
-                  </Input>
-                  <UncontrolledTooltip placement="top" target="endingTime">
-                    Ending time must be at least 2 hours after starting time!
-                  </UncontrolledTooltip>
-                </FormGroup>
-                <Button>Get planning!</Button>
-              </Form>
-            </Col>
-         </Row>
+      <Row>
+        <Col xs="12" sm={{ offset: 3, size: 6 }}>
+          {error &&
+            <Alert className="text-center" color="warning">
+              <strong>Whoops!</strong>
+              {' '}
+              Something happened on the server. Try again later
+            </Alert>}
+          {validItinerary &&
+            <Alert
+              className="text-center"
+              style={{ padding: '3px', height: '55px' }}
+              color="info"
+            >
+              <p style={{ marginBottom: '0px' }}>
+                It looks like you've already created an itinerary...
+              </p>
+              <p>
+                You can access it <Link
+                  to={`/itinerary-overview/${validItinerary.id}`}
+                >
+                  <strong>here</strong>
+                </Link>!
+              </p>
+
+            </Alert>}
+
+          <Hero />
+          <Form className="text-center" onSubmit={onSubmit}>
+            <FormGroup>
+              <Label for="startingLocation">Starting/Ending Location</Label>
+              <Input
+                style={{ maxWidth: '300px', margin: 'auto' }}
+                type="text"
+                name="startingLocation"
+                id="startingLocation"
+                placeholder="Use current location"
+              />
+            </FormGroup>
+            <div>
+              <FormGroup
+                style={{
+                  float: 'left',
+                  width: '40%',
+                  marginLeft: '8%',
+                  marginRight: '2%'
+                }}
+              >
+                <Label for="startingTime">Start Time</Label>
+                <Input
+                  style={{ maxWidth: '300px', margin: 'auto' }}
+                  type="select"
+                  name="startingTime"
+                  id="startingTime"
+                  onChange={onStartTimeChange}
+                >
+                  {createTimeOptions(nextHour, 0)}
+                </Input>
+              </FormGroup>
+              <FormGroup
+                style={{
+                  float: 'left',
+                  width: '40%',
+                  marginLeft: '2%',
+                  marginRight: '8%'
+                }}
+              >
+                <Label for="endingTime">End Time</Label>
+                <Input
+                  style={{ maxWidth: '300px', margin: 'auto' }}
+                  type="select"
+                  name="endingTime"
+                  id="endingTime"
+                  onChange={onEndTimeChange}
+                >
+                  {createTimeOptions(startTime, 2)}
+                </Input>
+                <UncontrolledTooltip placement="top" target="endingTime">
+                  Ending time must be at least 2 hours after starting time!
+                </UncontrolledTooltip>
+              </FormGroup>
+            </div>
+            <div>
+              <Button style={{ clear: 'both' }}>Get planning!</Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
     );
   }
 }
