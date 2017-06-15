af0bf5b460c083c600e6014420843565dcbac9cb
Egle Libby <eglelibby@gmail.com>
2017-06-15 11:41:36 -0400

    moved actions to proper reducers

--- client/src/actions/builderActions.js
+++ client/src/actions/builderActions.js
@@ -1,9 +1,6 @@
 import axios from "axios";
-import itineraryHelper from "../helpers/itineraryHelper";
-export const ADD_LOCATION_TO_ITINERARY = "ADD_LOCATION_TO_ITINERARY ";
-export const GET_FINAL_ITINERARY = "GET_FINAL_ITINERARY";
-export const SET_DURATION = "SET_DURATION";
-export const SET_FINAL_ITINERARY = "SET_FINAL_ITINERARY";
+import { getFinalItinerary } from "./itineraryActions";
+import { SET_DURATION } from "./types";
 
 export function addLocationToItinerary(
   location,
@@ -41,48 +38,5 @@ export function setDuration(data) {
   return {
     type: SET_DURATION,
     data
-  };
-}
-
-export function getFinalItinerary(itineraryId, history) {
-  return dispatch => {
-    axios
-      .get(`/api/itinerary/final/${itineraryId}`)
-      .then(response => {
-        if (response.status !== 200) {
-          throw new Error("Response not ok");
-        }
-        console.log("RESPONSE final itinerary", response);
-        itineraryHelper.setItineraryObj(itineraryId);
-        dispatch(setFinalItinerary(response.data));
-        history.push(`/itinerary-overview/${itineraryId}`);
-      })
-      .catch(function(error) {
-        console.log("Error:", error);
-      });
-  };
-}
-
-export function setFinalItinerary(data) {
-  return {
-    type: SET_FINAL_ITINERARY,
-    data
-  };
-}
-
-export function getSavedItinerary(itineraryId) {
-  return dispatch => {
-    axios
-      .get(`/api/itinerary/saved/${itineraryId}`)
-      .then(response => {
-        if (response.status !== 200) {
-          throw new Error("Response not ok");
-        }
-        console.log("Response to getting saved itinerary:", response);
-        dispatch(setFinalItinerary(response.data));
-      })
-      .catch(function(error) {
-        console.log("Error:", error);
-      });
   };
 }
