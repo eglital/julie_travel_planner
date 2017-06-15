ed53533f7ea26c05d7a662372e843ccc3331d1e5
Renzo Tomlinson <rttomlinson@gmail.com>
2017-06-15 14:52:17 +0000

    Add props for locationSegment

--- client/src/components/LocationSegment.js
+++ client/src/components/LocationSegment.js
@@ -1,16 +1,9 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
-import { Card } from 'reactstrap';
 
 
 
-let departureTime = Date.now();
-let arrivalTime = Date.now() + 100000000;
-let locationData = {
- url: "https://placeholdit.co//i/100x100",
- title: "BooBoo"
-};
 
 let timeboxStyle = {
  display: "flex",
@@ -18,16 +11,24 @@ let timeboxStyle = {
  justifyContent: "space-around",
  alignItems: "center",
  marginRight: 10,
+ minWidth: 75
 };
 
 
 
-const LocationSegment = () => {
+const LocationSegment = ({ arrivalTime, departureTime, locationData }) => {
+ departureTime = Date.now();
+arrivalTime = Date.now() + 100000000;
+locationData = {
+ url: "https://placeholdit.co//i/100x100",
+ title: "BooBoo Palace Burgers"
+};
+ 
  return (
         <div className="location-segment" style={{display: "flex", padding: 10, margin: 10, alignItems: "center"}}>
              <div style={timeboxStyle}><span>{moment(departureTime).format('LT')}</span><span>-</span><span>{moment(arrivalTime).format('LT')}</span></div>
-             <div style={{display: "flex", flexGrow: 1}}>
-              <img style={{maxWidth:75, maxHeight:75, flexShrink: 1}}src={locationData.url} alt={locationData.title}/>
+             <div className="location-segment-info" style={{display: "flex", flexGrow: 1}}>
+              <img style={{maxWidth:75, maxHeight:75}}src={locationData.url} alt={locationData.title}/>
               <p style={{flexGrow:1}}>{locationData.title}</p>
              </div>
         </div>
