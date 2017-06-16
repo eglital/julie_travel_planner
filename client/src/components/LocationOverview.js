521c284d1c92b2663104449f37e7ffd09993f574
Egle Libby <eglelibby@gmail.com>
2017-06-16 14:19:18 -0400

    added nicholas styles

--- client/src/components/LocationOverview.js
+++ client/src/components/LocationOverview.js
@@ -1,55 +1,118 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import LocationSegment from './LocationSegment';
-import TravelSegment from './TravelSegment';
-import GoogleMaps from './GoogleMaps';
-import { Link } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import LocationSegment from "./LocationSegment";
+import TravelSegment from "./TravelSegment";
+import GoogleMaps from "./GoogleMaps";
+import { Link } from "react-router-dom";
+import { Container, Row, Col, Button } from "reactstrap";
+import ShareTwitterButton from "./ShareTwitterButton";
+import ShareFacebookButton from "./ShareFacebookButton";
 
 function makeOverview(finalItinerary) {
- return finalItinerary.map((location, index) => {
-  let nextLocation = finalItinerary[index + 1];
-  if (index === finalItinerary.length - 1) {
-   return (
-    <div key={index}>
-      <LocationSegment arrivalTime={location.arrivalTime} departureTime={location.departureTime} locationData={{name: "Ending Location", photo: "https://placeholdit.co//i/555x150"}}/>
-     </div>
-   );
-  }
-  else if (index === 0) {
-   return (
-    <div key={index}>
-      <LocationSegment arrivalTime={location.arrivalTime} departureTime={location.departureTime} locationData={{name: "Starting Location", photo: "https://placeholdit.co//i/555x150"}}/>
-      <TravelSegment duration={nextLocation.arrivalTime - location.departureTime}/>
-     </div>
-   );
-  }
-  else {
-   return (
-    <div key={index}>
-      <LocationSegment arrivalTime={location.arrivalTime} departureTime={location.departureTime} locationData={{link: location.link, name: location.name, photo: location.photo, category: location.category}}/>
-      <TravelSegment duration={nextLocation.arrivalTime - location.departureTime}/>
-     </div>
-   );
-
-  }
- });
+  return finalItinerary.map((location, index) => {
+    let nextLocation = finalItinerary[index + 1];
+    if (index === finalItinerary.length - 1) {
+      return (
+        <div key={index}>
+          <LocationSegment
+            arrivalTime={location.arrivalTime}
+            departureTime={location.departureTime}
+            locationData={{
+              name: "Ending Location",
+              photo: "/map.png"
+            }}
+          />
+        </div>
+      );
+    } else if (index === 0) {
+      return (
+        <div key={index}>
+          <LocationSegment
+            arrivalTime={location.arrivalTime}
+            departureTime={location.departureTime}
+            locationData={{
+              name: "Starting Location",
+              photo: "/map.png"
+            }}
+          />
+          <TravelSegment
+            duration={nextLocation.arrivalTime - location.departureTime}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div key={index}>
+          <LocationSegment
+            arrivalTime={location.arrivalTime}
+            departureTime={location.departureTime}
+            locationData={{
+              link: location.link,
+              name: location.name,
+              photo: location.photo,
+              category: location.category
+            }}
+          />
+          <TravelSegment
+            duration={nextLocation.arrivalTime - location.departureTime}
+          />
+        </div>
+      );
+    }
+  });
 }
 
-const LocationOverview = ({
- finalItinerary
-}) => {
- return (
-  <div className="LocationOverview" style={{marginBottom: 20/*Should be equal to the height of the footer*/}}>
-   {makeOverview(finalItinerary)}
-   <Link to="/"><button>Plan a new route</button></Link>
-   <GoogleMaps finalItinerary={finalItinerary}/>
-  </div>
- );
+const LocationOverview = ({ finalItinerary }) => {
+  return (
+    <div
+      className="LocationOverview"
+      style={{
+        marginBottom: 20 /*Should be equal to the height of the footer*/
+      }}
+    >
+      <Row style={{ marginTop: "15px", marginBottom: "15px" }}>
+        <Col className="text-center">
+          <div>
+            <span>
+              Route by <h2
+                style={{
+                  width: "100%",
+                  color: "#C17DBF",
+                  marginBottom: "15px",
+                  textShadow: "-1px 0 grey, 0 1px grey, 1px 0 grey, 0 -1px grey"
+                }}
+              >
+                Julie
+              </h2>
+            </span>
+          </div>
+          <Link to="/">
+            <Button outline color="info" size="sm">Plan New Route</Button>
+          </Link>
+        </Col>
+      </Row>
+
+      {makeOverview(finalItinerary)}
+
+      <Row>
+        <Col xs="11" style={{ paddingRight: "0px" }}>
+          <GoogleMaps finalItinerary={finalItinerary} />
+        </Col>
+        <Col xs="1" style={{ paddingLeft: "0px" }}>
+          <div style={{ height: "30px" }} />
+          <ShareFacebookButton finalItinerary={finalItinerary} />
+          <div style={{ height: "30px" }} />
+
+          <ShareTwitterButton finalItinerary={finalItinerary} />
+        </Col>
+      </Row>
+
+    </div>
+  );
 };
 
 LocationOverview.propTypes = {
- finalItinerary: PropTypes.array
+  finalItinerary: PropTypes.array
 };
-
 
 export default LocationOverview;
--- client/src/components/TravelSegment.js
+++ client/src/components/TravelSegment.js
@@ -1,25 +1,26 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import PropTypes from "prop-types";
-import TimeHelper from '../helpers/timeHelper';
+import TimeHelper from "../helpers/timeHelper";
 
 var FA = require("react-fontawesome");
 
-
 const TravelSegment = ({ duration }) => {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <FA name="long-arrow-down fa-2x" stack="1x" />
-        </Col>
-        <Col>
-          <FA name="car fa-2x" />
-          {" "}
-          {TimeHelper.millisecondsToSeconds(duration)}{" "}min
-        </Col>
-      </Row>
-    </Container>
+    <Row>
+      <Col xs="3" />
+      <Col xs="9">
+        <div className="text-center">
+          <FA name="long-arrow-down fa" />
+          <FA
+            name="car fa"
+            style={{ marginRight: "15px", marginLeft: "15px" }}
+          />
+          {TimeHelper.millisecondsToSeconds(duration)}{" "}min{" "}
+        </div>
+
+      </Col>
+    </Row>
   );
 };
 
