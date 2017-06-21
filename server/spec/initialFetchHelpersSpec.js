c04dd040660880e8dc65e4c04b036bd3c52ee5c7
William Whitworth <william10charles@gmail.com>
2017-06-21 11:25:07 -0600

    Fixed bug in initial response test.

--- server/spec/initialFetchHelpersSpec.js
+++ server/spec/initialFetchHelpersSpec.js
@@ -28,52 +28,17 @@ describe("This is a section of testes for the initial four square request", () =
     done();
   });
 
-  describe("test api/itinerary/start response", () => {
-    beforeEach(done => {
-      originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-      jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
-
-      afterEach(() => {
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
-      });
-    });
-
-    xit(
-      "adds starts new itinerary in database and sends itinerary info",
-      done => {
-        request(
-          {
-            method: "POST",
-            url: `${apiUrl}/start`,
-
-            json: {
-              formSubmission: {
-                startTime: Date.now().valueOf(),
-                endTime: Date.now().valueOf() + 14400000,
-                startingLocation: [41.87574423890672, -87.6264445685823],
-                transportationMode: "driving"
-              }
-            }
-          },
-          (err, res, body) => {
-            console.log(body);
-            expect(body.itinerary.duration).toEqual(0);
-            done();
-          }
-        );
-      }
-    );
-  });
-
   describe("createItinary", () => {
     let dummy = {};
     dummy.startTime = Date.now();
     dummy.endTime = Date.now() + 14400000;
     dummy.lat = 45.7833;
     dummy.lng = -108.5007;
-    it("returns an Itinerary Object", function() {
+    const results = createItinary(dummy);
+    it("returns an Itinerary Object", function(done) {
       const results = createItinary(dummy);
       expect(results.data[0].lat).toEqual(45.7833);
+      done();
     });
     it("returns an Itinearary Object", function() {
       // const results = buildListOfChoices([]);
@@ -86,6 +51,31 @@ describe("This is a section of testes for the initial four square request", () =
       expect(function() {
         buildListOfChoices([1, 2, 3, 4]);
       }).toThrow(new Error("the data must be an array of arrays"));
+    });
+  });
+
+  describe("test api/itinerary/start response", () => {
+    it("adds starts new itinerary in database and sends itinerary info", done => {
+      request(
+        {
+          method: "POST",
+          url: `${apiUrl}/start`,
+
+          json: {
+            formSubmission: {
+              startTime: Date.now().valueOf(),
+              endTime: Date.now().valueOf() + 14400000,
+              startingLocation: [41.87574423890672, -87.6264445685823],
+              transportationMode: "driving"
+            }
+          }
+        },
+        (err, res, body) => {
+          console.log(body);
+          expect(body.itinerary.duration).toEqual(0);
+          done();
+        }
+      );
     });
   });
 });
