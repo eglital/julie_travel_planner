63fd7f0f9e87cc1cffe594916b1707fe95b713e2
Egle Libby <eglelibby@gmail.com>
2017-06-22 00:06:46 -0400

    moved logo to assets

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import LoginButton from './LoginButton';
-import LogoutButton from './LogoutButton';
-import fbAuthHelper from '../helpers/facebookAuthHelper';
-import { Navbar, Nav, NavItem, Collapse, NavbarToggler } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
+import fbAuthHelper from "../helpers/facebookAuthHelper";
+import { Navbar, Nav, NavItem, Collapse, NavbarToggler } from "reactstrap";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 class JulieNavbar extends React.Component {
   constructor() {
@@ -33,30 +34,30 @@ class JulieNavbar extends React.Component {
         <Navbar color="faded" light toggleable>
           <NavbarToggler right onClick={this.toggle} />
           <Link to="/">
-            <img src="./logo.png" alt="Julie Travel Planner" height="40px" />
+            <img src={logo} alt="Julie Travel Planner" height="40px" />
           </Link>
           <Collapse isOpen={this.state.isOpen} navbar>
             {this.state.userLoggedIn
               ? <Nav
                   className="ml-auto"
                   navbar
-                  style={{ textAlign: 'right', marginTop: '15px' }}
+                  style={{ textAlign: "right", marginTop: "15px" }}
                 >
                   <NavItem>
-                    <Link to="/saved" style={{ textDecoration: 'none' }}>
+                    <Link to="/saved" style={{ textDecoration: "none" }}>
                       <p className="nav-hover">
                         Itineraries
                       </p>
                     </Link>
                   </NavItem>
-                  <NavItem style={{ paddingLeft: '15px' }}>
+                  <NavItem style={{ paddingLeft: "15px" }}>
                     <LogoutButton />
                   </NavItem>
                 </Nav>
               : <Nav
                   className="ml-auto"
                   navbar
-                  style={{ textAlign: 'right', marginTop: '15px' }}
+                  style={{ textAlign: "right", marginTop: "15px" }}
                 >
                   <NavItem>
                     <LoginButton loginUser={this.loginUser} />
